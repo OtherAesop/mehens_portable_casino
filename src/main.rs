9864b711e90cbbfa65a720e33b676fcfdedb2f5f
Legion <goreian@pdx.edu>
2018-07-25 00:10:52 -0700

    Finished testing Dicecoin library

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ use gambling::dicecoins::Dicecoins;
 fn main() {
 
     let d4 = Dicecoins::new(vec![1,2,3,4]);
+    let d2 = Dicecoins::new(vec![4,2,1,3]);
 
     println!("Hello, world! Roll a standard d{}... result: {}", d4.face_count(), d4.roll_x(1));
     println!("Hello, world! Roll a standard d{}... max result: {}", d4.face_count(), d4.roll_max_x(1));
-    println!("Hello, world! Roll a standard d{}... max result: {}", d4.face_count(), d4.roll_avg_x(1));
+    println!("Hello, world! Roll a standard d{}... avg result: {}", d2.face_count(), d2.roll_avg_x(1));
     println!("Hello, world! Roll a standard d{}... min result: {}", d4.face_count(), d4.roll_min_x(1));
 }
